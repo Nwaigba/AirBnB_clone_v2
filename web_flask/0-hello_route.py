3f4f5ce3680e82d4905372912cadbd2cb7d8cb99
Nwaigba <ezehemmanuelval@gmail.com>
2023-08-17 17:45:13 +0000

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,11 +7,11 @@ from flask import Flask, render_template
 app = Flask("__name__")
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_flask():
     """Return string when route queried
     """
-    return render_template("10-hbnb_filters.html")
+    return "hello HBNB!"
 
 if __name__ == '__main__':
     app.url_map.strict_slashes = False
